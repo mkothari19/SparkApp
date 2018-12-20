f8249e23dc451989de0f0d37e52682159e298490
Maneesh Kothari <manishkothari1982@gmail.com>
2018-12-21 00:36:36 +0530

    spark codes changes

--- SparkApp/src/main/scala/dataframe/DataFrameCSV.scala
+++ SparkApp/src/main/scala/dataframe/DataFrameCSV.scala
@@ -1,6 +1,7 @@
 package dataframe
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
  
 object DataFrameCSV {
   def main(args: Array[String]): Unit = {
@@ -13,11 +14,13 @@ object DataFrameCSV {
    val data=spark.read.format("com.databricks.spark.csv").option("header","true").load(path) 
     data.show
     data.createOrReplaceTempView("Vehicle")
-    // Registration expire when fuel type is diesel and vehicle old then 1988 
-    
+    // Registration expire when fuel type is diesel and vehicle old then 10 years 
+    import spark.implicits._
     val fueltypedf=spark.sql("""select name,price,kilometer,brand,yearOfRegistration,fueltype from vehicle where fueltype!='null' and fueltype='diesel'  and (date_format(current_date(),"yyyy")-year(yearOfRegistration))>=10""")
-    
-   fueltypedf.show
-  
+   val dummydataColumn:(String)=>(String)=(data:String)=>{data}
+   val dummyColUDF= udf(dummydataColumn)
+   val updatedDF= fueltypedf.withColumn("registration",dummyColUDF(lit("cancel")))
+   updatedDF.show
+ updatedDF.write.mode("append").partitionBy("yearOfRegistration").parquet("/Volumes/MYHARDDRIVE/sparkdemoapp/SparkApp/dataset/output/")
   }
 }
