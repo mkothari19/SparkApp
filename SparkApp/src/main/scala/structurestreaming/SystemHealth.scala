8b8bfcd2d69b1be6750d53eeb5b04e26ad6c4980
Maneesh Kothari <manishkothari1982@gmail.com>
2018-12-26 23:36:39 +0530

    use withwatermarking

--- SparkApp/src/main/scala/structurestreaming/SystemHealth.scala
+++ SparkApp/src/main/scala/structurestreaming/SystemHealth.scala
@@ -18,7 +18,9 @@ object SystemHealth extends App with Context{
 val syshealthDS=  inputVal.map(x=>x.split("\t\t")).map(r=>syshealth(r(0),r(1),r(2),r(3)))
 val syshealthDF=syshealthDS.toDF()
 val finalDF=syshealthDF.select(unix_timestamp($"event","dd-MM-yyy HH:mm:ss").cast(TimestampType).as("timestamp"), syshealthDF.col("mem").cast("double"),syshealthDF.col("disk"),syshealthDF.col("cpu").cast("double"))
-val windowDF= finalDF.groupBy(window($"timestamp","10 seconds","5 seconds")).agg(avg($"mem").as("avgmem") ,max($"disk").as("maxdisk"),avg($"cpu").as("avgcpu"))
+val windowDF= finalDF.withWatermark("timestamp","10 minutes").
+              groupBy(window($"timestamp","10 seconds","5 seconds")).
+              agg(avg($"mem").as("avgmem") ,max($"disk").as("maxdisk"),avg($"cpu").as("avgcpu"))
 val query=windowDF.selectExpr("to_json(struct(*)) AS value").writeStream.format("kafka").option("kafka.bootstrap.servers","localhost:9092").
           option("topic","systemhealthout").option("checkpointLocation", "/Volumes/MYHARDDRIVE/checkpoint/systemhealth").outputMode(OutputMode.Complete()) .start()
 query.awaitTermination()
